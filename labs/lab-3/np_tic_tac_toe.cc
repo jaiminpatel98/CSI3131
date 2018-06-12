7775ff4ee6eb1d4a1c3e4a0d2c1e7580240383fe
Jaimin Patel <jaiminpatel98@gmail.com>
2018-06-12 09:58:52 -0400

    lab 3 refresh

--- labs/lab-3/np_tic_tac_toe.cc
+++ labs/lab-3/np_tic_tac_toe.cc
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include "tic_tac_toe.h"
+
 
 /* Usage example
  * 
@@ -26,8 +26,6 @@
  
 int main(int argc, char **argv) {
   char player;
-  char* my_pipe = "my_pipe";
-  mkfifo (my_pipe, 0666);
 
   if (argc != 2) {
     printf ("Usage: sig_tic_tac_toe [X|O] \n");
@@ -38,37 +36,10 @@ int main(int argc, char **argv) {
     printf ("Usage: player names must be either X or Y");
     return (-2);
   }
-  
-  char* my_fifo = "my_pipe";
-  tic_tac_toe *game = new tic_tac_toe();
-  char* state = game->convert2string();
-  game->set_game_state(state);
-  int turn = 0;
-  int file;
-  mkfifo(my_fifo, 0666);
-
-  if (player == 'O') {
-    turn = 1;
-  }
-
-  do {
-    if (turn & 1) {
-      file = open(my_fifo, O_RDONLY);
-      read(file, state, strlen(state) + 1);
-      close(file);
-      game->set_game_state(state);
-      game->display_game_board();
-      turn++;
-    } else {
-      file = open(my_fifo, O_WRONLY);
-      game->get_player_move(player);
-      state = game->convert2string();
-      write(file, state, strlen(state) + 1);
-      close(file);
-      game->display_game_board();
-      turn++;
-    }
-  } while ((player = game->game_result()) == '-');
-  printf ("Game finished, result: %c \n", player);
   return (0);
+
+  int file;
+  char str[128];
+  
+
 }
